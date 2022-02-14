2e2671a7846194c2c47f0def6eed9169a6343cd2
Ci-re <cirecleen@gmail.com>
2022-02-14 06:22:11 -0800

    src folder created

---
+++ src/components/Login_component.js
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function Login_component() {
+  return (
+    <View>
+      <Text>Login_component</Text>
+    </View>
+  )
+}

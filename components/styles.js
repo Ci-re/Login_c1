7c2698c6da2c14363ba79921440291ae1ba969bc
Ci-re <cirecleen@gmail.com>
2022-02-16 14:09:23 +0100

    update

--- components/styles.js
+++ components/styles.js
@@ -1,24 +1,40 @@
 import React from "react";
 import { StatusBar, StyleSheet } from "react-native";
 
-export const styles = StyleSheet.create({
-    container: {
-        flex : 1,
-        backgroundColor: "#A5A6F6",
-        paddingTop: StatusBar.currentHeight + 20,
-    },
-    innerContainer: {
-        flex: 1,
-        alignItems: "center",
-    },
-    logo_container : {
-        alignItems: "center",
-        height: 50,
-        width: 50,
-    },
-    logo_style : {
-        height: 100,
-        width: 100,
-    }
+export const colors = {
+  primary: "#ffffff",
+  secondary: "#000000",
+  tetiary: "#A5A6F6",
+  alternative: "#20263C",
+};
 
-})
+export const { primary, secondary, tetiary, alternative } = colors;
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: primary,
+    padding: 25,
+    paddingTop: StatusBar.currentHeight + 20,
+  },
+  innerContainer: {
+    flex: 1,
+    width: "100%",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  logo_container: {
+    height: 100,
+    width: 100,
+  },
+  logo_style: {
+    alignItems: "center",
+    height: 300,
+    width: 300,
+  },
+  get_started: {
+    backgroundColor: alternative,
+    height: 30,
+    width: 30,
+  },
+});
--- screens/Login_screens.js
+++ screens/Login_screens.js
@@ -1,15 +1,17 @@
-import { View, Text, Image } from 'react-native'
-import React from 'react'
-import {styles} from "../components/styles"
+import { View, Text, Image, TouchableOpacity } from "react-native";
+import React from "react";
+import { styles } from "../components/styles";
 
 export default function Login_screens() {
   return (
     <View style={styles.container}>
       <View style={styles.innerContainer}>
-        <View style={styles.logo_container}>
-          <Image source={require("../assets/images/logo.png")} style={styles.logo_style}></Image>
-        </View>
+        <Image source={require("../assets/images/onboard-image.png")} />
+        <Text>Earn while you save</Text>
+        <TouchableOpacity style={styles.get_started}>
+          <Text>Click me</Text>
+        </TouchableOpacity>
       </View>
     </View>
-  )
+  );
 }
